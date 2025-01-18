722897078fe0f7949e289c7d2c1cd72a368b7bff
Chris <chris.catignani@gmail.com>
2025-01-18 16:02:53 +0100

    Adding support for other OneWorld partners

--- src/utils/calculators/earnCategories.js
+++ src/utils/calculators/earnCategories.js
@@ -1,5 +1,43 @@
+import { getAirport } from "../airports"
+
 export const getPartnerEarnCategory = (segment) => {
-  return partnerEarnCategories[segment.airline].fareBuckets[segment.fareClass]
+  for(let rule of partnerEarnCategories[segment.airline].fareBuckets.rules) {
+    if (!isApplicableRule(segment, rule)) {
+      continue
+    }
+
+    if (rule.notSupported) {
+      throw new Error(rule.notSupported.reason)
+    }
+
+    return rule.categories[segment.fareClass]
+
+  }
+}
+
+const isApplicableRule = (segment, rule) => {
+  const check = (originAirport, destinationAirport, rule) => {
+    if (rule.all) {
+      return true
+    }
+
+    let originApplies = false
+    let destinationApplies = false
+
+    if (rule.origin.country) {
+      originApplies = rule.origin.country.has(originAirport.country.toLowerCase())
+    }
+
+    if (rule.destination.country) {
+      destinationApplies = rule.destination.country.has(destinationAirport.country.toLowerCase())
+    }
+
+    return originApplies && destinationApplies
+  }
+
+  const originAirport = getAirport(segment.fromAirport)
+  const destinationAirport = getAirport(segment.toAirport)
+  return check(originAirport, destinationAirport, rule) || check(destinationAirport, originAirport, rule)
 }
 
 /**
@@ -7,7 +45,7 @@ export const getPartnerEarnCategory = (segment) => {
  */
 const buildFareBuckets = (qantasString) => {
   // remove all '*', replace whitespace with single space, split on that single space
-  const fareBuckets = qantasString.trim().replace(/\*/gm, '').replace(/\s+/gm, ' ').toLowerCase().split(' ')
+  const fareBuckets = qantasString.trim().replace(/\*\~\^/gm, '').replace(/\s+/gm, ' ').toLowerCase().split(' ')
   const fareClasses = ['discountEconomy', 'economy', 'flexibleEconomy', 'premiumEconomy', 'business', 'first']
   const retval = {}
 
@@ -23,6 +61,35 @@ const buildFareBuckets = (qantasString) => {
   return retval
 }
 
+const buildSimpleFareBuckets = (qantasString) => {
+  return {
+    rules: [
+      {
+        all: true,
+        categories: buildFareBuckets(qantasString)
+      }
+    ]
+  }
+}
+
+const buildJapanAirlinesFareBuckets = (qantasString) => {
+  return {
+    rules: [
+      {
+        origin: { country: new Set(['japan']) },
+        destination: { country: new Set(['japan']) },
+        notSupported: {
+          reason: 'Intra Japan flights on JAL are not yet supported'
+        }
+      },
+      {
+        all: true,
+        categoreis: buildFareBuckets(qantasString)
+      }
+    ]
+  }
+}
+
 const partnerEarnCategories = {
   'aa': {
     'statusMultipliers': {
@@ -32,16 +99,59 @@ const partnerEarnCategories = {
       'platinum': 1.00,
       'platinum one': 1.00
     },
-    'fareBuckets': buildFareBuckets('NOQ 	GKLMSV	HY	PW 	CDIJR	AF '),
+    'fareBuckets': buildSimpleFareBuckets('NOQ 	GKLMSV	HY	PW 	CDIJR	AF ')
+  },
+  'as': {
+    'fareBuckets': buildSimpleFareBuckets('GOQX	KLMNSV	BHY	-	CDIJ~	AF')
   },
   'ba': {
-    'fareBuckets': buildFareBuckets('GKLMNOQSV	-	BEHTWY	-	CDIRJ	AF')
+    'fareBuckets': buildSimpleFareBuckets('GKLMNOQSV	-	BEHTWY	-	CDIRJ	AF')
   },
   'ay': {
-    'fareBuckets': buildFareBuckets('AZ	GLMNOQSV	BHKY	EPTW	CDIJR	-')
+    'fareBuckets': buildSimpleFareBuckets('AZ	GLMNOQSV	BHKY	EPTW	CDIJR	-')
+  },
+  'ib': {
+    'fareBuckets': buildSimpleFareBuckets('AFGNOQZ	KLMSV	BHY	ETW 	CDIJR	-')
+  },
+  'i2': {
+    'fareBuckets': buildSimpleFareBuckets('AFGNOQZ	KLMSV	BHY	ETW 	CDIJR	-')
+  },
+  'cx': {
+    'fareBuckets': buildSimpleFareBuckets('ML	BHK	YE	RW	CDIJP	AF')
   },
   'qr': {
-    'fareBuckets': buildFareBuckets('KLMV	BH 	Y	-	CDIJP*R	AF')
+    'fareBuckets': buildSimpleFareBuckets('KLMV	BH 	Y	-	CDIJP*R	AF')
+  },
+  'at': {
+    'fareBuckets':buildSimpleFareBuckets('NOQRSTW	HKLMV	BY	-	CDIJ	-')
+  },
+  'rj': {
+    'fareBuckets': buildSimpleFareBuckets('VSNQOPW	KML	BYH	IZ*	CDJ	-')
+  },
+  'jl': {
+    'fareBuckets': buildJapanAirlinesFareBuckets('GNOQZ^	HKLMSV	BY	EWPR	CDIJX	AF')
+  },
+  'nu': {
+    'fareBuckets': buildJapanAirlinesFareBuckets('GNOQZ^	HKLMSV	BY	EWPR	CDIJX	AF')
+  },
+  'mh': { // TODO additional rules apply for malaysia
+    'fareBuckets': {
+      'k': 'discountEconomy',
+      'l': 'discountEconomy',
+      'm': 'discountEconomy',
+      'v': 'discountEconomy',
+
+      'b': 'economy',
+      'h': 'economy',
+
+      'i': 'flexibleEconomy',
+      'y': 'flexibleEconomy',
+      'z': 'flexibleEconomy',
+
+      'c': 'business',
+      'd': 'business',
+      'j': 'business'
+    }
   },
   'ul': { // TODO additional rules apply for sri lanka
     'fareBuckets': {
@@ -69,27 +179,5 @@ const partnerEarnCategories = {
       'i': 'business',
       'j': 'business'
     }
-  },
-  'mh': { // TODO additional rules apply for malaysia
-    'fareBuckets': {
-      'k': 'discountEconomy',
-      'l': 'discountEconomy',
-      'm': 'discountEconomy',
-      'v': 'discountEconomy',
-
-      'b': 'economy',
-      'h': 'economy',
-
-      'i': 'flexibleEconomy',
-      'y': 'flexibleEconomy',
-      'z': 'flexibleEconomy',
-
-      'c': 'business',
-      'd': 'business',
-      'j': 'business'
-    }
-  },
-  'cx': {
-    'fareBuckets': buildFareBuckets('ML	BHK	YE	RW	CDIJP	AF')
   }
 }
