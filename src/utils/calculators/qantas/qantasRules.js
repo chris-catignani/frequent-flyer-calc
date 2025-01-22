54ff5636d7a69147aacc6fec8bf377289368bc39
Chris <chris.catignani@gmail.com>
2025-01-22 19:42:28 +0800

    Add some more qantas rules

--- src/utils/calculators/qantas/qantasRules.js
+++ src/utils/calculators/qantas/qantasRules.js
@@ -1,4 +1,4 @@
-import { parseEarningRates, IntraCountryRule } from "../rules"
+import { parseEarningRates, IntraCountryRule, GeographicalRule } from "../rules"
 import { QANTAS_FARE_CLASSES } from "./qantasEarnCategories"
 
 const _base_rule_url = 'https://www.qantas.com/es/en/frequent-flyer/earn-points/airline-earning-tables/qantas-and-jetstar-earning-tables.html'
@@ -6,6 +6,9 @@ const _base_rule_url = 'https://www.qantas.com/es/en/frequent-flyer/earn-points/
 export const getQantasRules = () => {
   return [
     buildIntraAustraliaRule(),
+    buildAdlBneGoldCoastSydMelRule(),
+    buildDarwinPerthRule(),
+    buildNewZealandRule(),
   ]
 }
 
@@ -34,4 +37,78 @@ const buildIntraAustraliaRule = () => {
 
   const ruleUrl = _base_rule_url + '#domestic-australia-and-new-zealand'
   return new IntraCountryRule('Domestic Australia', ruleUrl, 'Australia', distanceBands)
+}
+
+const buildAdlBneGoldCoastSydMelRule = () => {
+  const ruleConfig = {
+    origin: {
+      city: new Set(['adelaide', 'brisbane', 'coolangatta', 'sydney', 'melbourne'])
+    },
+    destination: {
+      city: {
+        delhi: parseQantasEarningRates('3,600	5,400	7,200	7,200	9,000	9,800	10,700	11,600	12,500	14,300', '40	50	75	75	80	85	150	155	165	230'),
+        bangalore: parseQantasEarningRates('3,600	5,400	7,200	7,200	9,000	9,800	10,700	11,600	12,500	14,300', '40	50	75	75	80	85	150	155	165	230'),
+        johannesburg: parseQantasEarningRates('3,750	5,625	7,500	7,500	9,375	10,300	11,250	12,200	13,125	15,000', '40	55	80	80	85	90	160	165	175	240'),
+        santiago: parseQantasEarningRates('3,750	5,625	7,500	7,500	9,375	10,300	11,250	12,200	13,125	15,000', '40	55	80	80	85	90	160	165	175	240'),
+        dallas: parseQantasEarningRates('4,900	7,350	9,800	9,800	12,250	13,400	14,700	15,950	17,200	19,600', '50	70	100	100	105	115	200	210	220	300'),
+        dubai: parseQantasEarningRates('4,500	6,750	9,000	9,000	11,250	12,400	13,500	14,625	15,750	18,000', '45	60	90	90	100	115	180	190	200	270'),
+      },
+      country: {
+        'new zealand': parseQantasEarningRates('1,000	1,375	1,750	1,750	2,125	2,300	2,500	2,700	2,875	3,250', '20	25	40	40	45	50	80	85	90	120'),
+        'papua new guinea': parseQantasEarningRates('1,000	1,375	1,750	1,750	2,125	2,300	2,500	2,700	2,875	3,250', '20	25	40	40	45	50	80	85	90	120'),
+      },
+      region: {
+        northeastAsia: parseQantasEarningRates('2,600	3,900	5,200	5,200	6,500	7,200	7,800	8,450	9,100	10,400', '30	40	60	60	65	70	120	125	135	180'),
+        southeastAsia: parseQantasEarningRates('2,600	3,900	5,200	5,200	6,500	7,200	7,800	8,450	9,100	10,400', '30	40	60	60	65	70	120	125	135	180'),
+        hawaii: parseQantasEarningRates('3,000	4,500	6,000	6,000	7,500	8,250	9,000	9,750	10,500	12,000', '35	45	70	70	75	80	140	150	160	210'),
+        usaWestCoast: parseQantasEarningRates('4,500	6,750	9,000	9,000	11,250	12,400	13,500	14,625	15,750	18,000', '45	60	90	90	100	115	180	190	200	270'),
+        usaEastCoast: parseQantasEarningRates('6,200	9,300	12,400	12,400	15,500	17,000	18,600	20,150	21,700	24,800', '70	95	140	140	150	160	280	295	310	420'),
+        europe: parseQantasEarningRates('6,200	9,300	12,400	12,400	15,500	17,000	18,600	20,150	21,700	24,800', '70	95	140	140	150	160	280	295	310	420'),
+      }
+    }
+  }
+
+  const ruleUrl = _base_rule_url + '#between-adl-bne-ool-mel-syd-and-'
+  return new GeographicalRule('Adelaide, Brisbane, Gold Coast, Melbourne, Sydney', ruleUrl, ruleConfig)
+}
+
+const buildDarwinPerthRule = () => {
+  const ruleConfig = {
+    origin: {
+      city: new Set(['darwin', 'perth'])
+    },
+    destination: {
+      city: {
+        dubai: parseQantasEarningRates('3,000	4,500	6,000	6,000	7,500	8,250	9,000	9,750	10,500	12,000', '35	45	70	70	75	80	140	150	160	210'),
+      },
+      region: {
+        northeastAsia: parseQantasEarningRates('2,500	3,750	5,000	5,000	6,250	6,900	7,500	8,125	8,750	10,000', '30	40	60	60	65	70	120	125	135	180'),
+        southeastAsia: parseQantasEarningRates('1.450	2,025	2,700	2,700	3,375	3,725	4,050	4,400	4,725	5,400', '25	25	50	50	50	50	100	100	100	150'),
+        europe: parseQantasEarningRates('4,700	7,050	9,400	9,400	11,750	12,850	14,100	15,300	16,450	18,800', '60	80	120	120	130	140	240	255	270	360'),
+      }
+    }
+  }
+
+  const ruleUrl = _base_rule_url + '#between-darwin-perth-and'
+  return new GeographicalRule('Darwin, Perth', ruleUrl, ruleConfig)
+}
+
+const buildNewZealandRule = () => {
+  const ruleConfig = {
+    origin: {
+      country: new Set(['new zealand'])
+    },
+    destination: {
+      city: {
+        dallas: parseQantasEarningRates('4,250	6,375	8,500	8,500	10,625	11,625	12,750	13,835	14,920	17,000', '45	60	85	85	90	100	170	180	190	260'),
+        santiago: parseQantasEarningRates('2,900	4,350	5,800	5,800	7,250	8,000	8,700	9,450	10,150	11,600', '35	45	70	70	70	70	140	150	160	210'),
+      },
+      region: {
+        usaEastCoast: parseQantasEarningRates('5,200	7,925	10,650	10,650	13,375	14,700	16,100	17,450	18,825	21,550', '50	70	100	100	105	110	200	210	220	300'),
+      }
+    }
+  }
+
+  const ruleUrl = _base_rule_url + '#between-new-zealand-and'
+  return new GeographicalRule('New Zealand', ruleUrl, ruleConfig)
 }
