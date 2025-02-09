e3becfb5c9d8990fc7709100746e96863015990d
Chris <chris.catignani@gmail.com>
2025-02-09 14:23:45 +0800

    fixing date format for Qantas API

--- src/app/api/qantas/route.js
+++ src/app/api/qantas/route.js
@@ -3,12 +3,14 @@ import { NextResponse } from "next/server";
 // Qantas API endpoint
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
-  const url = buildFQantasUrl(
+  const url = buildQantasUrl(
     searchParams.get('airline'),
     searchParams.get('fromIata'),
     searchParams.get('toIata'),
     searchParams.get('eliteStatus')
   );
+
+  console.log(`Calling qantas with url: ${url}`)
 
   const resp = await fetch(url);
   const respJson = await resp.json()
@@ -17,7 +19,7 @@ export async function GET(req) {
 
 // Build the request url, e.g.
 // https://api.services.qantasloyalty.com/earnquote/v1/rewards?fares=AA_LHRJFK&tiers=Bronze&date=2025-01-29
-function buildFQantasUrl(airline, fromIata, toIata, eliteStatus){
+function buildQantasUrl(airline, fromIata, toIata, eliteStatus){
   const date = new Date();
   const qantasUrl = new URL(
     "https://api.services.qantasloyalty.com/earnquote/v1/rewards"
@@ -28,10 +30,15 @@ function buildFQantasUrl(airline, fromIata, toIata, eliteStatus){
     airline.toUpperCase() + "_" + fromIata.toUpperCase() + toIata.toUpperCase()
   );
   qantasUrl.searchParams.append("tiers", eliteStatus);
-  qantasUrl.searchParams.append(
-    "date",
-    date.getFullYear() + "-" + date.getMonth() + 1 + "-" + date.getDate()
-  );
+  qantasUrl.searchParams.append("date", formatDate(date));
 
   return qantasUrl.href;
+};
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
