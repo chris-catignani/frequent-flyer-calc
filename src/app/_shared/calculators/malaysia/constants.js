452d4d912e225b66ade4aef33604f01377096752
Chris <chris.catignani@gmail.com>
2025-05-08 17:00:35 +0200

    parse malaysia airlines rules instead of hard code

--- src/app/_shared/calculators/malaysia/constants.js
+++ src/app/_shared/calculators/malaysia/constants.js
@@ -1,163 +1,75 @@
 // https://enrich.malaysiaairlines.com/enrich/partners.html
 
+const buildFareClassObj = (data) => {
+  const fareClassObj = {
+    fare_class: {},
+    earning: {},
+  };
+
+  data.forEach(([fareClasses, fareCategory, earningRate]) => {
+    fareClasses.split('/').forEach((fareClass) => {
+      fareClassObj['fare_class'][fareClass.trim().toLowerCase()] = fareCategory;
+      fareClassObj['earning'][fareClass.trim().toLowerCase()] = earningRate;
+    });
+  });
+
+  return fareClassObj;
+};
+
 export const MALAYSIA_FARE_CLASSES = {
+  //TODO parse these cleaner
+
   aa: {
-    fare_class: {
-      f: 'first',
-      a: 'first',
-      p: 'first',
-
-      j: 'business',
-      d: 'business',
-      r: 'business',
-      i: 'business',
-
-      y: 'economy',
-      h: 'economy',
-      k: 'economy',
-      m: 'economy',
-      w: 'economy',
-      l: 'economy',
-      v: 'economy',
-      g: 'economy',
-      s: 'economy',
-
-      n: 'none',
-      o: 'none',
-      q: 'none',
-    },
-    earning: {
-      f: 1.5,
-      a: 1.5,
-      p: 1.25,
-
-      j: 1.25,
-      d: 1.25,
-      r: 1.25,
-      i: 1.25,
-
-      y: 1.0,
-      h: 0.75,
-      k: 0.5,
-      m: 0.5,
-      w: 0.5,
-      l: 0.5,
-      v: 0.5,
-      g: 0.25,
-      s: 0.25,
-
-      n: 0,
-      o: 0,
-      q: 0,
-    },
+    ...buildFareClassObj([
+      ['F / A', 'first', 1.5],
+      ['P', 'first', 1.25],
+      ['J / D / R / I', 'business', 1.25],
+      ['Y', 'economy', 1],
+      ['H', 'economy', 0.75],
+      ['K / M / W / L / V', 'economy', 0.5],
+      ['S / G', 'economy', 0.25],
+      ['N / O / Q', 'none', 0.0],
+    ]),
   },
   ay: {
-    fare_class: {
-      j: 'business',
-      c: 'business',
-      d: 'business',
-      r: 'business',
-      i: 'business',
-
-      y: 'economy',
-      b: 'economy',
-      h: 'economy',
-
-      v: 'economy',
-      l: 'economy',
-      n: 'economy',
-      s: 'economy',
-      q: 'economy',
-      o: 'economy',
-      k: 'economy',
-      m: 'economy',
-      z: 'economy',
-
-      g: 'economy',
-    },
-    earning: {
-      j: 1.25,
-      c: 1.25,
-      d: 1.25,
-      r: 1.0,
-      i: 1.0,
-
-      y: 1.0,
-      b: 1.0,
-      h: 1.0,
-
-      v: 0.5,
-      l: 0.5,
-      n: 0.5,
-      s: 0.5,
-      q: 0.5,
-      o: 0.5,
-      k: 0.5,
-      m: 0.5,
-      z: 0.5,
-
-      g: 0.25,
-    },
+    ...buildFareClassObj([
+      ['J / C / D ', 'business', 1.25],
+      ['I / R', 'business', 1.0],
+      ['Y / B / H', 'economy', 1.0],
+      ['V / L / N / S / Q / O / K / M / Z', 'economy', 0.5],
+      ['G', 'economy', 0.25],
+    ]),
   },
   ba: {
-    fare_class: {
-      f: 'first',
-      a: 'first',
-
-      j: 'business',
-      c: 'business',
-      d: 'business',
-      r: 'business',
-      i: 'business',
-
-      w: 'economy',
-      e: 'economy',
-      t: 'economy',
-
-      y: 'economy',
-      b: 'economy',
-      h: 'economy',
-
-      k: 'economy',
-      m: 'economy',
-
-      l: 'economy',
-      v: 'economy',
-      s: 'economy',
-      n: 'economy',
-      q: 'economy',
-      o: 'economy',
-      g: 'economy',
-    },
-    earning: {
-      f: 1.5,
-      a: 1.5,
-
-      j: 1.25,
-      c: 1.25,
-      d: 1.25,
-      r: 1.25,
-      i: 1.0,
-
-      w: 1.0,
-      e: 1.0,
-      t: 1.0,
-
-      y: 1.0,
-      b: 1.0,
-      h: 1.0,
-
-      k: 0.5,
-      m: 0.5,
-
-      l: 0.25,
-      v: 0.25,
-      s: 0.25,
-      n: 0.25,
-      q: 0.25,
-      o: 0.25,
-      g: 0.25,
-    },
+    ...buildFareClassObj([
+      ['F / A', 'first', 1.5],
+      ['J / C / D / R', 'business', 1.25],
+      ['I', 'business', 1.0],
+      ['W / E / T', 'economy', 1.0],
+      ['Y / B / H', 'economy', 1.0],
+      ['K / M', 'economy', 0.5],
+      ['L / V / S / N / Q / O / G', 'economy', 0.25],
+    ]),
+  },
+  cx: {
+    ...buildFareClassObj([
+      ['F / A', 'first', 1.5],
+      ['J / C / D / P / I', 'business', 1.25],
+      ['W / R / E', 'economy', 1.1],
+      ['Y / B', 'economy', 1.0],
+      ['H / K / M', 'economy', 0.5],
+    ]),
+  },
+  qr: {
+    ...buildFareClassObj([
+      ['F / A', 'first', 1.5],
+      ['J / C / D / I', 'business', 1.25],
+      ['R', 'business', 1.0],
+      ['P', 'business', 0.75],
+      ['Y / B / H', 'economy', 1.0],
+      ['K / M', 'economy', 0.5],
+      ['L / V / S / N / T / Q / O / G / W', 'economy', 0.25],
+    ]),
   },
 };
 
@@ -165,4 +77,6 @@ export const MALAYSIA_RULE_URLS = {
   aa: 'https://enrich.malaysiaairlines.com/enrich/partners/airlines/oneworld-alliances/american-airlines.html',
   ay: 'https://enrich.malaysiaairlines.com/enrich/partners/airlines/oneworld-alliances/finnair.html',
   ba: 'https://enrich.malaysiaairlines.com/enrich/partners/airlines/oneworld-alliances/british-airways.html',
+  cx: 'https://enrich.malaysiaairlines.com/enrich/partners/airlines/oneworld-alliances/cathay-pacific.html',
+  qr: 'https://enrich.malaysiaairlines.com/enrich/partners/airlines/oneworld-alliances/qatar-airways.html',
 };
